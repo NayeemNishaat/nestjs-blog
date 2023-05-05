91cb38e5ba920606738804906242c6a0f12c8ab7
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-05-05 14:31:09 +0600

    tested like/dislike endpoints

--- test/article.e2e-spec.ts
+++ test/article.e2e-spec.ts
@@ -2,14 +2,17 @@ import { Test, TestingModule } from "@nestjs/testing";
 import { INestApplication } from "@nestjs/common";
 import request from "supertest";
 import { ArticleModule } from "../src/modules/article/article.module";
+import { UserModule } from "../src/modules/user/user.module";
 import { MongooseModule } from "@nestjs/mongoose";
 import { ThrottlerModule } from "@nestjs/throttler";
 import { ArticleService } from "../src/modules/article/article.service";
+import { UserService } from "../src/modules/user/user.service";
 import { ConfigModule } from "@nestjs/config";
 
 describe("ArticleController (e2e)", () => {
   let app: INestApplication;
   let articleService: ArticleService;
+  let userService: UserService;
 
   beforeAll(async () => {
     const moduleRef: TestingModule = await Test.createTestingModule({
@@ -27,7 +30,8 @@ describe("ArticleController (e2e)", () => {
             uri: process.env.MONGO_URI
           })
         }),
-        ArticleModule
+        ArticleModule,
+        UserModule
       ]
     }).compile();
 
@@ -35,6 +39,7 @@ describe("ArticleController (e2e)", () => {
     await app.init();
 
     articleService = moduleRef.get<ArticleService>(ArticleService);
+    userService = moduleRef.get<UserService>(UserService);
   });
 
   afterAll(async () => {
@@ -44,6 +49,7 @@ describe("ArticleController (e2e)", () => {
   afterEach(async () => {
     // Note: Trancate DB
     await articleService.deleteAllArticle();
+    await userService.deleteAllUser();
   });
 
   it("/article (POST)", async () => {
@@ -94,38 +100,107 @@ describe("ArticleController (e2e)", () => {
     expect(body.data._id).toEqual(article._id.toString());
   });
 
-  // it("/user/:id (PATCH)", async () => {
-  //   const user = await userService.createUser({
-  //     firstName: "John",
-  //     lastName: "Doe",
-  //     email: "abc@gmail.com"
-  //   });
-  //   const email = "new@mail.com";
+  it("/article/like (PATCH)", async () => {
+    const article = await articleService.createArticle({
+      name: "My Article",
+      body: "This is my article",
+      author: "6439383e06b3b43356b39e4f",
+      categories: ["tech", "science"],
+      tags: ["Ok", "Good"]
+    });
 
-  //   await request(app.getHttpServer())
-  //     .patch(`/user/${user._id.toString()}`)
-  //     .set("Content-Type", "application/json")
-  //     .send({ email })
-  //     .expect(200);
+    await request(app.getHttpServer())
+      .patch(`/article/like`)
+      .set("Content-Type", "application/json")
+      .send({
+        articleId: article._id.toString(),
+        userId: "6439383e06b3b43356b39e4f"
+      })
+      .expect(200);
 
-  //   const updatedUser = await userService.getUserById(user._id.toString());
+    const likedArticle = await articleService.getArticleById(
+      article._id.toString()
+    );
 
-  //   expect(updatedUser.email).toEqual(email);
-  // });
+    expect(likedArticle._id).toEqual(article._id);
+    expect(likedArticle.likes).toEqual(1);
+  });
 
-  // it("/user/:id (DELETE)", async () => {
-  //   const user = await userService.createUser({
-  //     firstName: "John",
-  //     lastName: "Doe",
-  //     email: "abc@gmail.com"
-  //   });
+  it("/article/dislike (PATCH)", async () => {
+    const article = await articleService.createArticle({
+      name: "My Article",
+      body: "This is my article",
+      author: "6439383e06b3b43356b39e4f",
+      categories: ["tech", "science"],
+      tags: ["Ok", "Good"]
+    });
 
-  //   await request(app.getHttpServer())
-  //     .delete(`/user/${user._id.toString()}`)
-  //     .expect(200);
+    await request(app.getHttpServer())
+      .patch(`/article/dislike`)
+      .set("Content-Type", "application/json")
+      .send({
+        articleId: article._id.toString(),
+        userId: "6439383e06b3b43356b39e4f"
+      })
+      .expect(200);
 
-  //   const retrieveUser = await userService.getUserById(user._id.toString());
+    const dislikedArticle = await articleService.getArticleById(
+      article._id.toString()
+    );
 
-  //   expect(retrieveUser).toBe(null);
-  // });
+    expect(dislikedArticle._id).toEqual(article._id);
+    expect(dislikedArticle.dislikes).toEqual(1);
+  });
+
+  it("likes and then dislikes an article by the same user", async () => {
+    // Part: Create New User
+    const user = await userService.createUser({
+      email: "Myemail@gmail.com",
+      firstName: "Nayee,",
+      lastName: "Nishaat"
+    });
+
+    // Part: Create New Article
+    const article = await articleService.createArticle({
+      name: "My Article",
+      body: "This is my article",
+      author: user._id.toString(),
+      categories: ["tech", "science"],
+      tags: ["Ok", "Good"]
+    });
+
+    // Part: Like an Article
+    await request(app.getHttpServer())
+      .patch(`/article/like`)
+      .set("Content-Type", "application/json")
+      .send({
+        articleId: article._id.toString(),
+        userId: user._id.toString()
+      })
+      .expect(200);
+
+    const likedArticle = await articleService.getArticleById(
+      article._id.toString()
+    );
+
+    expect(likedArticle.likes).toEqual(1);
+    expect(likedArticle.dislikes).toEqual(0);
+
+    // Part: Dislike an Article by the Same User
+    await request(app.getHttpServer())
+      .patch(`/article/dislike`)
+      .set("Content-Type", "application/json")
+      .send({
+        articleId: article._id.toString(),
+        userId: user._id.toString()
+      })
+      .expect(200);
+
+    const dislikedArticle = await articleService.getArticleById(
+      article._id.toString()
+    );
+
+    expect(dislikedArticle.dislikes).toEqual(1);
+    expect(dislikedArticle.likes).toEqual(0);
+  });
 });
